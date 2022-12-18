7143af7c92ab98a5fa1ef8c8d2c7221d9b536fe9
Clayton Stupi <945509+cstupi@users.noreply.github.com>
2022-12-18 12:34:22 -0800

    puzzle1 done

--- 2022/day6/puzzle1/src/main.rs
+++ 2022/day6/puzzle1/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    let inputtext = include_str!("input.txt").chars().collect::<Vec<char>>();
+    let mut count = 0;
+    let window = 14;
+    for w in inputtext.as_slice().windows(window) {
+        println!("{:?}", w);
+        let mut unique = true;
+        let mut i = 0;
+        let mut j = 1;
+        while i < window {
+            j = i+1;
+            while j < window {
+                println!("{:?} != {:?}", w[i], w[j]);
+                if(w[i] == w[j]) {
+                    unique = false;
+                }
+                j += 1;
+            }
+            i += 1;
+        }    
+
+        if unique {
+            println!("{}", count+window);
+            return;
+        } 
+        count += 1;
+    }
+    println!("None found: {:?}", count);
 }
